78e846b64c76f7cae02efc1e57fbee9b854b95a0
rogeriokrychak <roger.krychak@gmail.com>
2021-11-06 00:17:46 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_CRUD_2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Projeto_CRUD_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTeste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTeste
+            // 
+            this.btnTeste.Location = new System.Drawing.Point(117, 98);
+            this.btnTeste.Name = "btnTeste";
+            this.btnTeste.Size = new System.Drawing.Size(159, 29);
+            this.btnTeste.TabIndex = 0;
+            this.btnTeste.Text = "Teste de Conexao";
+            this.btnTeste.UseVisualStyleBackColor = true;
+            this.btnTeste.Click += new System.EventHandler(this.btnTeste_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTeste);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTeste;
+    }
+}
+
---
+++ Projeto_CRUD_2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_CRUD_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeste_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection connect = new SQLiteConnection("Data Source = C:\\bancos\\Banco_CSharp.db; Version=3;");
+            connect.Open();//abre a conexão com o banco de dados
+            MessageBox.Show("Conexao OK");
+            
+           
+            SQLiteCommand command = connect.CreateCommand();//cria o comando para criar o comando select
+            //criar o comando para inserir os dados no banco de dados
+            command.CommandText = "INSERT INTO FILMES(NOME, AUTOR, DIRETOR, DATA_LANCAMENTO, GENERO) VALUES (@nome, @autor, @diretor, @data, @genero)";
+            command.Parameters.AddWithValue("@nome", "Matrix");//essa chamada de comando ela chama a coluna e troca os parametros escritos aqui
+            command.Parameters.AddWithValue("@autor", "ZYX");
+            command.Parameters.AddWithValue("@diretor", "ZYC");
+            command.Parameters.AddWithValue("@data", "2000-01-11");
+            command.Parameters.AddWithValue("@genero", "Ficção");
+            command.Prepare();
+            command.ExecuteNonQuery();
+            command.CommandText = "select *from filmes";
+            SQLiteDataReader result = command.ExecuteReader();
+            MessageBox.Show("itens localizados:" + result.GetValues().Count);
+            while(result.Read())
+            {
+                int id = result.GetInt32(0);
+                string nome = result.GetString(1);
+                string autor = result.GetString(2);
+                string diretor = result.GetString(3);
+                string data = result.GetDateTime(4).ToString();
+                string genero = result.GetString(5);
+                MessageBox.Show(id.ToString() + " - " + nome + " - " + autor + " - " + diretor + " - " + data + " - " + genero);
+            }
+            
+        }
+    }
+}
