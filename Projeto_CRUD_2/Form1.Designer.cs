72526f49fa587769afb52d23d9d5c49eac5ea4eb
rogeriokrychak <roger.krychak@gmail.com>
2021-11-13 00:31:19 -0300

    INSERIDO COMANDOS PARA ALTERAR

--- Projeto_CRUD_2/Form1.Designer.cs
+++ Projeto_CRUD_2/Form1.Designer.cs
@@ -30,11 +30,36 @@ namespace Projeto_CRUD_2
         private void InitializeComponent()
         {
             this.btnTeste = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.campoID = new System.Windows.Forms.TextBox();
+            this.campoDataLancamento = new System.Windows.Forms.TextBox();
+            this.campoGenero = new System.Windows.Forms.TextBox();
+            this.campoDiretor = new System.Windows.Forms.TextBox();
+            this.campoAutor = new System.Windows.Forms.TextBox();
+            this.campoNome = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diretor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data_lancamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnTeste
             // 
-            this.btnTeste.Location = new System.Drawing.Point(117, 98);
+            this.btnTeste.Location = new System.Drawing.Point(915, 409);
             this.btnTeste.Name = "btnTeste";
             this.btnTeste.Size = new System.Drawing.Size(159, 29);
             this.btnTeste.TabIndex = 0;
@@ -42,21 +67,270 @@ namespace Projeto_CRUD_2
             this.btnTeste.UseVisualStyleBackColor = true;
             this.btnTeste.Click += new System.EventHandler(this.btnTeste_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Autor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Diretor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Gênero:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Data de Lançamento:";
+            // 
+            // campoID
+            // 
+            this.campoID.Location = new System.Drawing.Point(13, 37);
+            this.campoID.Name = "campoID";
+            this.campoID.Size = new System.Drawing.Size(247, 27);
+            this.campoID.TabIndex = 7;
+            // 
+            // campoDataLancamento
+            // 
+            this.campoDataLancamento.Location = new System.Drawing.Point(14, 311);
+            this.campoDataLancamento.Name = "campoDataLancamento";
+            this.campoDataLancamento.Size = new System.Drawing.Size(247, 27);
+            this.campoDataLancamento.TabIndex = 8;
+            // 
+            // campoGenero
+            // 
+            this.campoGenero.Location = new System.Drawing.Point(14, 249);
+            this.campoGenero.Name = "campoGenero";
+            this.campoGenero.Size = new System.Drawing.Size(247, 27);
+            this.campoGenero.TabIndex = 9;
+            // 
+            // campoDiretor
+            // 
+            this.campoDiretor.Location = new System.Drawing.Point(14, 196);
+            this.campoDiretor.Name = "campoDiretor";
+            this.campoDiretor.Size = new System.Drawing.Size(247, 27);
+            this.campoDiretor.TabIndex = 10;
+            // 
+            // campoAutor
+            // 
+            this.campoAutor.Location = new System.Drawing.Point(13, 143);
+            this.campoAutor.Name = "campoAutor";
+            this.campoAutor.Size = new System.Drawing.Size(247, 27);
+            this.campoAutor.TabIndex = 11;
+            // 
+            // campoNome
+            // 
+            this.campoNome.Location = new System.Drawing.Point(13, 90);
+            this.campoNome.Name = "campoNome";
+            this.campoNome.Size = new System.Drawing.Size(247, 27);
+            this.campoNome.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Pesquisar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nome,
+            this.autor,
+            this.diretor,
+            this.data_lancamento,
+            this.genero});
+            this.dataGridView1.Location = new System.Drawing.Point(285, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(779, 301);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome";
+            this.nome.MinimumWidth = 6;
+            this.nome.Name = "nome";
+            this.nome.Width = 125;
+            // 
+            // autor
+            // 
+            this.autor.HeaderText = "Autor";
+            this.autor.MinimumWidth = 6;
+            this.autor.Name = "autor";
+            this.autor.Width = 125;
+            // 
+            // diretor
+            // 
+            this.diretor.HeaderText = "Diretor";
+            this.diretor.MinimumWidth = 6;
+            this.diretor.Name = "diretor";
+            this.diretor.Width = 125;
+            // 
+            // data_lancamento
+            // 
+            this.data_lancamento.HeaderText = "Data";
+            this.data_lancamento.MinimumWidth = 6;
+            this.data_lancamento.Name = "data_lancamento";
+            this.data_lancamento.Width = 125;
+            // 
+            // genero
+            // 
+            this.genero.HeaderText = "Gênero";
+            this.genero.MinimumWidth = 6;
+            this.genero.Name = "genero";
+            this.genero.Width = 125;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Relatório";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Alterar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(485, 409);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Excluir";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1076, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.campoNome);
+            this.Controls.Add(this.campoAutor);
+            this.Controls.Add(this.campoDiretor);
+            this.Controls.Add(this.campoGenero);
+            this.Controls.Add(this.campoDataLancamento);
+            this.Controls.Add(this.campoID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnTeste);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FORMULÁRIO CADASTRO DE FILMES";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnTeste;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox campoID;
+        private System.Windows.Forms.TextBox campoDataLancamento;
+        private System.Windows.Forms.TextBox campoGenero;
+        private System.Windows.Forms.TextBox campoDiretor;
+        private System.Windows.Forms.TextBox campoAutor;
+        private System.Windows.Forms.TextBox campoNome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn autor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diretor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data_lancamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genero;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Projeto_CRUD_2/Form1.cs
+++ Projeto_CRUD_2/Form1.cs
@@ -13,19 +13,19 @@ namespace Projeto_CRUD_2
 {
     public partial class Form1 : Form
     {
+        SQLiteConnection connect = new SQLiteConnection("Data Source = C:\\bancos\\Banco_CSharp.db; Version=3;");
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnTeste_Click(object sender, EventArgs e)
+        private void btnTeste_Click(object sender, EventArgs e)//inserção de dados de forma estática
         {
-            SQLiteConnection connect = new SQLiteConnection("Data Source = C:\\bancos\\Banco_CSharp.db; Version=3;");
+            
             connect.Open();//abre a conexão com o banco de dados
             MessageBox.Show("Conexao OK");
-            
-           
-            SQLiteCommand command = connect.CreateCommand();//cria o comando para criar o comando select
+            /*SQLiteCommand command = connect.CreateCommand();//cria o comando para criar o comando select
             //criar o comando para inserir os dados no banco de dados
             command.CommandText = "INSERT INTO FILMES(NOME, AUTOR, DIRETOR, DATA_LANCAMENTO, GENERO) VALUES (@nome, @autor, @diretor, @data, @genero)";
             command.Parameters.AddWithValue("@nome", "Matrix");//essa chamada de comando ela chama a coluna e troca os parametros escritos aqui
@@ -38,7 +38,7 @@ namespace Projeto_CRUD_2
             command.CommandText = "select *from filmes";
             SQLiteDataReader result = command.ExecuteReader();
             MessageBox.Show("itens localizados:" + result.GetValues().Count);
-            while(result.Read())
+           /* while(result.Read())
             {
                 int id = result.GetInt32(0);
                 string nome = result.GetString(1);
@@ -47,8 +47,107 @@ namespace Projeto_CRUD_2
                 string data = result.GetDateTime(4).ToString();
                 string genero = result.GetString(5);
                 MessageBox.Show(id.ToString() + " - " + nome + " - " + autor + " - " + diretor + " - " + data + " - " + genero);
+            }*/
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            connect.Open();//abre a conexão com o banco de dados
+            MessageBox.Show("Conexao OK");
+            SQLiteCommand command = connect.CreateCommand();//cria o comando para criar o comando select
+            //criar o comando para inserir os dados no banco de dados
+            command.CommandText = "INSERT INTO FILMES(NOME, AUTOR, DIRETOR, DATA_LANCAMENTO, GENERO) VALUES (@nome, @autor, @diretor, @data, @genero)";
+            command.Parameters.AddWithValue("@nome", campoNome.Text);//essa chamada de comando ela chama a coluna e troca os parametros escritos aqui
+            command.Parameters.AddWithValue("@autor", campoAutor.Text);
+            command.Parameters.AddWithValue("@diretor", campoDiretor.Text);
+            command.Parameters.AddWithValue("@data", campoDataLancamento.Text);
+            command.Parameters.AddWithValue("@genero", campoGenero.Text);
+            command.Prepare();
+            command.ExecuteNonQuery();
+            connect.Close();
+                MessageBox.Show("Cadastro inserido!!!");
+            }catch (Exception)
+            {
+                MessageBox.Show("Falha ao Salvar!!!");
             }
             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connect.Open();//abre a conexão com o banco de dados
+            SQLiteCommand command = connect.CreateCommand();//cria o comando para criar o comando select
+            command.CommandText = "select *from filmes where id = @id";//faz um select ou consulta pelo id
+            command.Parameters.AddWithValue("@id", campoID.Text);
+            SQLiteDataReader result = command.ExecuteReader();
+           
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    campoNome.Text = result.GetString("nome");
+                    campoAutor.Text = result.GetString("autor");
+                    campoDiretor.Text = result.GetString("diretor");
+                    campoDataLancamento.Text = result.GetString("data_lancamento");
+                    campoGenero.Text = result.GetString("genero");
+                }
+            } else
+            {
+                MessageBox.Show("Id não está cadastrado!!!!");
+            }
+            connect.Close();//fecha a conexão com o banco de dados
+        }
+
+        private void button3_Click(object sender, EventArgs e)//comando para fazer o relatório
+        {
+            connect.Open();//abre a conexão com o banco de dados
+            SQLiteCommand command = connect.CreateCommand();//cria o comando para manipular os dados
+            command.CommandText = "select *from filmes where id like id order by id ";//faz um select
+            //ou consulta e mostra um relatório dentro do gridview pelo id em ordem decrescente
+            SQLiteDataReader result = command.ExecuteReader();
+            dataGridView1.Rows.Clear();//limpa as linhas de consulta dentro do gridview
+            while (result.Read())
+            {
+                dataGridView1.Rows.Add(new string[] {
+                    result.GetInt32("id").ToString(),
+                    result.GetString("nome"),
+                    result.GetString("autor"),
+                    result.GetString("diretor"),
+                    result.GetString("data_lancamento"),
+                    result.GetString("genero")
+
+                });
+            }
+            connect.Close();//fecha a conexão com o banco de dados
+        }
+
+        private void button4_Click(object sender, EventArgs e)// este é o comando para alterar os dados
+        {
+            try
+            {
+                connect.Open();//abre a conexão com o banco de dados
+                MessageBox.Show("Conexao OK");
+                SQLiteCommand command = connect.CreateCommand();
+                command.CommandText = "update filmes set nome=@nome,autor=@autor,diretor=@diretor,data_lancamento=@data,genero=@genero where id=@id";
+                command.Parameters.AddWithValue("id", campoID.Text);
+                command.Parameters.AddWithValue("@nome", campoNome.Text);
+                command.Parameters.AddWithValue("@autor", campoAutor.Text);
+                command.Parameters.AddWithValue("@diretor", campoDiretor.Text);
+                command.Parameters.AddWithValue("@data", campoDataLancamento.Text);
+                command.Parameters.AddWithValue("@genero", campoGenero.Text);
+                command.Prepare();
+                command.ExecuteNonQuery();
+                
+                MessageBox.Show("Cadastro alterado!!!");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Falha ao Alterar!!!");
+            }
+            connect.Close();//fecha a conexão com o banco
         }
     }
 }
